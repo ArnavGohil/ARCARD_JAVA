18ec84e1dfc9a9748bc9abe99a80cdcc8d3a16ae
Arnav <arnav.gohil04@gmail.com>
2020-02-15 15:43:17 +0530

    Initial commit

---
+++ src/main/java/com/example/MetroVR/MainActivity.java
@@ -0,0 +1,26 @@
+package com.example.MetroVR;
+
+import android.app.ActivityOptions;
+import android.content.Intent;
+import android.os.Build;
+import android.os.Bundle;
+import android.view.View;
+import androidx.annotation.RequiresApi;
+import androidx.appcompat.app.AppCompatActivity;
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
+    public void Hi(View view)
+    {
+        startActivity(new Intent(this , UnityPlayerActivity.class ),
+                ActivityOptions.makeSceneTransitionAnimation(MainActivity.this).toBundle());
+    }
+}
